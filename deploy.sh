95274f5136cc53f3f51ea4a04d2dde66909c0a05
Heng Zhang <zhangheng1536@gmail.com>
2018-04-02 12:44:13 +0800

    add travis deploy

--- deploy.sh
+++ deploy.sh
@@ -2,7 +2,7 @@
 echo "11111\n"
 
 echo "\n$0\n----------\n$1\n"
-openssl aes-256-cbc -K $0 -iv $1 -in tron.enc -out tron -d
+openssl aes-256-cbc -K $1 -iv $2 -in tron.enc -out tron -d
 
 echo "22222\n"
 ssh vivider@198.35.44.132 -p 28290 mkdir aaaaa
