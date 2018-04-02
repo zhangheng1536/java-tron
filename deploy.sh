6eddf1997b1dd959230b8cf267914c1e3542285d
Heng Zhang <zhangheng1536@gmail.com>
2018-04-02 16:39:19 +0800

    add travis deploy

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 ssh vivider@198.35.44.132 -p 28290 mkdir aaaaa
-ssh vivider@198.35.44.132 -p 28290 echo "ppppppp" > aasaa.txt
-ls
+ssh vivider@198.35.44.132 -p 28290 touch bbbbb.txt
+ls -a
