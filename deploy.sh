9619359dd262e6a99faefb11fc6bcfead29d98d9
Heng Zhang <zhangheng1536@gmail.com>
2018-04-02 12:22:21 +0800

    add travis deploy

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
-openssl aes-256-cbc -K $0 -iv $1 in tron.enc -out tron -d
+openssl aes-256-cbc -K $0 -iv $1 -in tron.enc -out tron -d
+
 ssh vivider@198.35.44.132 -p 28290 mkdir aaaaa
 ssh vivider@198.35.44.132 -p 28290 echo "ppppppp" >aasaa.txt
