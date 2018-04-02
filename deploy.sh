d892334d557118584129cca8d17fe4040b7b1467
Heng Zhang <zhangheng1536@gmail.com>
2018-04-02 16:59:25 +0800

    add travis deploy

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
-ssh vivider@198.35.44.132 -p 28290 mkdir aaaaaaa
-ssh vivider@198.35.44.132 -p 28290 touch bbbbb.txt
+ssh vivider@198.35.44.132 -p 28290 mkdir aaaaaaaa
+ssh vivider@198.35.44.132 -p 28290 touch bbbbbp.txt
+scp -P 28290 core vivider@198.35.44.132:~/aaaaaaaa
 ls -a
