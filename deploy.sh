b0295445705b6a4036d77ae260d0b5b22b7a9773
Heng Zhang <zhangheng1536@gmail.com>
2018-04-03 21:10:39 +0800

    add travis deploy

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 ./gradlew clean shadowJar
-# ssh vivider@198.35.44.132 -p 28290 touch bbb.txt
+# ssh vivider@198.35.44.132 -p 28290 touch bbbbb.txt
+ssh vivider@198.35.44.132 -p 28290 mkdir tron
 scp -P 28290 build/libs/java-tron.jar vivider@198.35.44.132:/home/vivider/tron/
 scp -P 28290 start.sh vivider@198.35.44.132:/home/vivider/tron/
 ssh vivider@198.35.44.132 -p 28290 sh tron/start.sh
